101aa9e1c3cc4f788ac895fb7d0b382049042064
Stephen Coakley <me@stephencoakley.com>
2015-09-08 14:46:52 -0500

    More options and better completions

--- apt.fish
+++ apt.fish
@@ -8,10 +8,10 @@
 function apt -d "Short and friendly command wrapper for APT"
     # Check for super cow powers.
     if type -f -q cowsay
-        set -g __APT_COW 1
-        set -g __APT_COW_MESSAGE "This APT has Super Cow Powers."
+        set -g __apt_cow 1
+        set -g __apt_cow_message "This APT has Super Cow Powers."
     else
-        set -g __APT_COW_MESSAGE "This APT does not have Super Cow Powers."
+        set -g __apt_cow_message "This APT does not have Super Cow Powers."
     end
 
     # If --help, -h, or no command given, display usage message.
@@ -20,18 +20,38 @@ function apt -d "Short and friendly command wrapper for APT"
         return 0
     end
 
+    # Parse command options.
+    set i (contains -i -- -d $argv; or contains -i -- --dry-run $argv)
+    if test -n "$i"
+        set -e argv[$i]
+
+        set dry_run 1
+        set apt_get_flags $apt_get_flags "-s"
+    end
+    set -e i
+
+    set i (contains -i -- -y $argv; or contains -i -- --force-yes $argv)
+    if test -n "$i"
+        set -e argv[$i]
+
+        set force_yes 1
+        set apt_get_flags $apt_get_flags "-y"
+        set ppa_purge_flags $ppa_purge_flags "-y"
+    end
+
+    # Parse and execute the given command.
     switch $argv[1]
         case autoremove
-            sudo apt-get autoremove
+            sudo apt-get $apt_get_flags autoremove
 
         case check
-            sudo apt-get check
+            sudo apt-get $apt_get_flags check
 
         case clean
-            sudo apt-get autoclean
+            sudo apt-get $apt_get_flags autoclean
 
         case install in
-            sudo apt-get install $argv[2..-1]
+            sudo apt-get $apt_get_flags install $argv[2..-1]
 
         case policy
             env LANG=C apt-cache policy
@@ -42,34 +62,39 @@ function apt -d "Short and friendly command wrapper for APT"
                 return 1
             end
 
+            if set -q dry_run
+                echo "Dry runs are not supported for PPA commands."
+                return 1
+            end
+
             switch $argv[2]
             case list
-                for APT in (find /etc/apt/ -name \*.list)
-                    grep -o "^deb http://ppa.launchpad.net/[a-z0-9\-]\+/[a-z0-9\-]\+" $APT | while read ENTRY
-                        set USER (echo $ENTRY | cut -d/ -f4)
-                        set PPA (echo $ENTRY | cut -d/ -f5)
-                        echo $USER/$PPA
+                for apt in (find /etc/apt/ -name \*.list)
+                    grep -o "^deb http://ppa.launchpad.net/[a-z0-9\-]\+/[a-z0-9\-]\+" $apt | while read entry
+                        set user (echo $entry | cut -d/ -f4)
+                        set ppa (echo $entry | cut -d/ -f5)
+                        echo $user/$ppa
                     end
                 end
 
+            case purge
+                sudo ppa-purge $ppa_purge_flags ppa:$argv[3]
+
             case remove
                 sudo add-apt-repository --remove ppa:$argv[3]
-
-            case purge
-                sudo ppa-purge ppa:$argv[3]
 
             case '*'
                 sudo add-apt-repository ppa:$argv[2]
             end
 
         case purge
-            sudo apt-get --purge remove $argv[2..-1]
+            sudo apt-get $apt_get_flags --purge remove $argv[2..-1]
 
         case reconfigure
             sudo dpkg-reconfigure $argv[2..-1]
 
         case remove rm
-            sudo apt-get remove $argv[2..-1]
+            sudo apt-get $apt_get_flags remove $argv[2..-1]
 
         case search
             apt-cache search $argv[2..-1]
@@ -78,20 +103,20 @@ function apt -d "Short and friendly command wrapper for APT"
             apt-cache show $argv[2..-1]
 
         case update up
-            sudo apt-get update
+            sudo apt-get $apt_get_flags update
 
         case upgrade
             if set -q argv[2]; and test $argv[2] = dist
-                sudo apt-get dist-upgrade
+                sudo apt-get $apt_get_flags dist-upgrade
             else
-                sudo apt-get upgrade
+                sudo apt-get $apt_get_flags upgrade
             end
 
         case moo
-            if set -q __APT_COW
+            if set -q __apt_cow
                 cowsay "Have you mooed today?"
             else
-                echo $__APT_COW_MESSAGE
+                echo $__apt_cow_message
             end
 
         case '*'
@@ -106,7 +131,9 @@ function __apt_help
 Usage: apt COMMAND [OPTIONS] [arg...]
 
 Options:
-    -h, --help  Display this help message and exit.
+    -d, --dry-run   Simulate the result of a command without taking action.
+    -h, --help      Display this help message and exit.
+    -y, --force-yes Do not prompt and assume yes for all queries.
 
 Commands:
     autoremove  Remove automatically all unused packages.
@@ -123,5 +150,5 @@ Commands:
     update      [Alias: up] Updates the list of downloadable packages.
     upgrade     Upgrades packages.
 
-$__APT_COW_MESSAGE"
+$__apt_cow_message"
 end
--- completions/apt.fish
+++ completions/apt.fish
@@ -1,1 +1,35 @@
-complete -c apt -a "autoremove check clean install in policy ppa purge reconfigure remove rm search show update up upgrade"
+complete -c apt -n "not __fish_use_subcommand" -s d -l dry-run
+complete -c apt -n "not __fish_use_subcommand" -s h -l help
+complete -c apt -n "not __fish_use_subcommand" -s y -l force-yes
+
+complete -c apt -n "__fish_use_subcommand" -a autoremove -f
+
+complete -c apt -n "__fish_use_subcommand" -a check -f
+
+complete -c apt -n "__fish_use_subcommand" -a clean -f
+
+complete -c apt -n "__fish_use_subcommand" -a "install in" -x
+
+complete -c apt -n "__fish_use_subcommand" -a policy -f
+
+complete -c apt -n "__fish_use_subcommand" -a ppa -x
+complete -c apt -n "__fish_seen_subcommand_from ppa" -a "list purge remove" -f
+
+complete -c apt -n "__fish_use_subcommand" -a purge -x
+
+complete -c apt -n "__fish_use_subcommand" -a reconfigure -x
+complete -c apt -n "__fish_seen_subcommand_from reconfigure" -a "(__fish_print_packages)" -f
+
+complete -c apt -n "__fish_use_subcommand" -a "remove rm" -x
+complete -c apt -n "__fish_seen_subcommand_from remove" -a "(__fish_print_packages)" -f
+complete -c apt -n "__fish_seen_subcommand_from rm" -a "(__fish_print_packages)" -f
+
+complete -c apt -n "__fish_use_subcommand" -a search -x
+
+complete -c apt -n "__fish_use_subcommand" -a show -x
+complete -c apt -n "__fish_seen_subcommand_from show" -a "(__fish_print_packages)" -f
+
+complete -c apt -n "__fish_use_subcommand" -a "update up" -f
+
+complete -c apt -n "__fish_use_subcommand" -a upgrade -f
+complete -c apt -n "__fish_seen_subcommand_from upgrade" -a "(__fish_print_packages)" -f
